e52f9f2f07104c1def8718ea4102476655831dba
Valeri Bilshevych <valera12bravis@gmail.com>
2024-07-13 02:25:49 +0300

    Update example

---
+++ example/vite.config.js
@@ -0,0 +1,23 @@
+import legacy from '@vitejs/plugin-legacy';
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv) => {
+  return ({
+    plugins: [
+      react(),
+      legacy({
+        modernPolyfills: ['es/global-this']
+      }),
+    ],
+    build: {
+      sourcemap: true,
+      reportCompressedSize: true
+    },
+    server: {
+      host: true,
+      port: 5657
+    }
+  });
+});
